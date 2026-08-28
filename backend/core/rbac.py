d0a525d2037e08e671b2f335d458f23022e04075
Greg Drake <greg@madmallards.com>
2026-08-27 23:59:03 -0400

    feat: add subscription feature permissions

--- backend/core/rbac.py
+++ backend/core/rbac.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import SAFE_METHODS, BasePermission
 
+from .entitlements import has_feature
+
 PLATFORM_ADMIN = "Platform Administrator"
 CLOUD_ADMIN = "Cloud Administrator"
 FINOPS_ANALYST = "FinOps Analyst"
@@ -31,3 +33,46 @@ class GovernancePermission(BasePermission):
 class PlatformAdminPermission(BasePermission):
     def has_permission(self, request, view):
         return user_has_role(request.user, {PLATFORM_ADMIN})
+
+
+class FeatureEntitlementPermission(BasePermission):
+    message = "Your current subscription does not include this feature."
+
+    def has_permission(self, request, view):
+        if not request.user or not request.user.is_authenticated:
+            return False
+        if request.user.is_superuser:
+            return True
+        feature = getattr(view, "required_feature", None)
+        return bool(feature and has_feature(request.user, feature))
+
+
+class ComplianceEntitlementPermission(FeatureEntitlementPermission):
+    def has_permission(self, request, view):
+        view.required_feature = "compliance"
+        return super().has_permission(request, view)
+
+
+class PolicyEntitlementPermission(FeatureEntitlementPermission):
+    def has_permission(self, request, view):
+        view.required_feature = "policies"
+        return super().has_permission(request, view)
+
+
+class BudgetEntitlementPermission(FeatureEntitlementPermission):
+    def has_permission(self, request, view):
+        view.required_feature = "budgets"
+        return super().has_permission(request, view)
+
+
+class RecommendationEntitlementPermission(FeatureEntitlementPermission):
+    def has_permission(self, request, view):
+        view.required_feature = "recommendations"
+        return super().has_permission(request, view)
+
+
+class RemediationEntitlementPermission(FeatureEntitlementPermission):
+    def has_permission(self, request, view):
+        feature = "remediation_live" if request.method not in SAFE_METHODS else "remediation_simulation"
+        view.required_feature = feature
+        return super().has_permission(request, view)
