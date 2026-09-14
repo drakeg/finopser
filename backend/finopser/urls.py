d7606395c08613f20b4e08e3770dcb01cf799251
Greg Drake <greg@madmallards.com>
2026-09-13 22:03:23 -0400

    feat: mount versioned public API

--- backend/finopser/urls.py
+++ backend/finopser/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/", include("core.public_api_urls")),
     path("api/", include("core.urls")),
 ]
