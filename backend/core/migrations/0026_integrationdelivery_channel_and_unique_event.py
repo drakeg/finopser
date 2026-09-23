51b15c90f884af48d30c61e15d9f23e7971b680e
Greg Drake <greg@madmallards.com>
2026-09-22 23:43:19 -0400

    fix: reference notification channel migration

--- backend/core/migrations/0026_integrationdelivery_channel_and_unique_event.py
+++ backend/core/migrations/0026_integrationdelivery_channel_and_unique_event.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("core", "0025_notificationchannel"),
+        ("core", "0025_notification_channel"),
     ]
 
     operations = [
