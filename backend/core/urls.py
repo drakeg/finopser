6ea8f8c597a84c91f96e4c992e821e21c9edfa3d
Greg Drake <greg@madmallards.com>
2026-09-23 19:58:39 -0400

    feat: route OIDC authorization requests

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -67,6 +67,7 @@ urlpatterns = [
     path("auth/logout/", views.logout, name="logout"),
     path("auth/me/", views.me, name="me"),
     path("auth/sso/discover/", identity_api.discover, name="enterprise-identity-discover"),
+    path("auth/sso/oidc/authorize/", identity_api.begin_oidc_authorization, name="enterprise-identity-oidc-authorize"),
     path("enterprise-identity/", identity_api.configuration, name="enterprise-identity-configuration"),
     path("integrations/destinations/", integration_destination_api.destinations, name="integration-destinations"),
     path("integrations/destinations/<int:pk>/", integration_destination_api.destination_detail, name="integration-destination-detail"),
